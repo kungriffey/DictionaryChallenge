fd51e169b33c8a67dc0cc38c5c5717395214ef4b
kunwardeep <kunwardeep.gill@gmail.com>
2015-01-27 18:38:03 -0500

    Initial Commit

---
+++ UserDataChallengeSolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserDataChallengeSolution
+//
+//  Created by Kunwardeep Gill on 2015-01-27.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserDataChallengeSolution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserDataChallengeSolution
+//
+//  Created by Kunwardeep Gill on 2015-01-27.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
