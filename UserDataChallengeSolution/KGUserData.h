51b4b440bd02f2430ed91ba0e6f2b4a8131d3041
kunwardeep <kunwardeep.gill@gmail.com>
2015-01-28 11:46:10 -0500

    first commit

---
+++ UserDataChallengeSolution/KGUserData.h
@@ -0,0 +1,13 @@
+//
+//  KGUserData.h
+//  UserDataChallengeSolution
+//
+//  Created by Kunwardeep Gill on 2015-01-27.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KGUserData : NSObject
+
+@end
---
+++ UserDataChallengeSolution/KGUserData.m
@@ -0,0 +1,13 @@
+//
+//  KGUserData.m
+//  UserDataChallengeSolution
+//
+//  Created by Kunwardeep Gill on 2015-01-27.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "KGUserData.h"
+
+@implementation KGUserData
+
+@end
